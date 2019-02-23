3836cae587a7b8e9f93cbee69e6d2cd9be19c605
Galileo2010 <33446456+Galileo2010@users.noreply.github.com>
2019-02-24 00:08:01 +0800

    编写实现计费功能代码 2019-2-24 00:07:54

--- main/main.js
+++ main/main.js
@@ -1,4 +1,5 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(miles, minutes) {
+    let sum = minutes * 0.25;
+    sum = (miles <= 8) ? sum + miles * 0.8 : sum + 8 * 0.8 + (miles - 8) * 0.8 * (1 + 0.5);
+    return Math.round(sum);
 };
