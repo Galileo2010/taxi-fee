a54dca9a37a333b54ec1d69233d25026fe57a374
Galileo2010 <33446456+Galileo2010@users.noreply.github.com>
2019-02-24 00:07:01 +0800

    安装依耐，编写测试代码 2019-2-24 00:06:59

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,32 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it("case 1 : 4 km 0 minutes",function(){
+        let gold = 4 * 0.8 + 0 * 0.25;
+        gold = Math.round(gold);
+        if(gold !== main(4,0)){
+            throw new Error("should return " + gold);
+        }
+    })
+    it("case 2 : 5 km 5 minutes",function(){
+        let gold = 5 * 0.8 + 5 * 0.25;
+        gold = Math.round(gold);
+        if(gold !== main(5,5)){
+            throw new Error("should return " + gold);
+        }
+    })
+    it("case 3 : 9 km 0 minutes",function(){
+        let gold =  8 * 0.8 + 1 * 1.2 + 0 * 0.25;
+        gold = Math.round(gold);
+        if(gold !== main(9,0)){
+            throw new Error("should return " + gold);
+        }
+    })
+    it("case 4 : 9 km 5 minutes",function(){
+        let gold = 8 * 0.8 + 1 * 1.2 + 5 * 0.25;
+        gold = Math.round(gold);
+        if(gold !== main(9,5)){
+            throw new Error("should return " + gold);
+        }
+    })
 });
