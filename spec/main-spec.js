8b49c5dc88b819a759ec3902df7260c4494e1fe8
Galileo2010 <33446456+Galileo2010@users.noreply.github.com>
2019-02-24 12:15:45 +0800

    use expect.toequal 2019-2-24 12:15:41

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,32 +1,24 @@
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    it("case 1 : 4 km 0 minutes",function(){
+    it("case 1 : 4 km 0 minutes", ()=>{
         let gold = 4 * 0.8 + 0 * 0.25;
         gold = Math.round(gold);
-        if(gold !== main(4,0)){
-            throw new Error("should return " + gold);
-        }
+        expect(main(4,0)).toEqual(gold);
     })
-    it("case 2 : 5 km 5 minutes",function(){
+    it("case 2 : 5 km 5 minutes", ()=>{
         let gold = 5 * 0.8 + 5 * 0.25;
         gold = Math.round(gold);
-        if(gold !== main(5,5)){
-            throw new Error("should return " + gold);
-        }
+        expect(main(5,5)).toEqual(gold);
     })
-    it("case 3 : 9 km 0 minutes",function(){
+    it("case 3 : 9 km 0 minutes", ()=>{
         let gold =  8 * 0.8 + 1 * 1.2 + 0 * 0.25;
         gold = Math.round(gold);
-        if(gold !== main(9,0)){
-            throw new Error("should return " + gold);
-        }
+        expect(main(9,0)).toEqual(gold);
     })
-    it("case 4 : 9 km 5 minutes",function(){
+    it("case 4 : 9 km 5 minutes", ()=>{
         let gold = 8 * 0.8 + 1 * 1.2 + 5 * 0.25;
         gold = Math.round(gold);
-        if(gold !== main(9,5)){
-            throw new Error("should return " + gold);
-        }
+        expect(main(9,5)).toEqual(gold);
     })
 });
